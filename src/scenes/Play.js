149e18f4603b8d40a724f223ffe8cafcac1a5cef
Zasteinme <zackarydsteinmetz@gmail.com>
2021-04-06 18:46:56 -0700

    First Day

---
+++ src/scenes/Play.js
@@ -0,0 +1,10 @@
+//const { Phaser } = require("../../Library/phaser");
+
+class Play extends Phaser.Scene {
+    constructor(){
+        super("playScene");
+    }
+    create(){
+        this.add.text(20, 20, "Rocket Patrol Play");
+    }
+}
