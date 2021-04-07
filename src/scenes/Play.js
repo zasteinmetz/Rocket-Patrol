53f2422100e3eef21354f7844cb2bded5e752346
Zasteinme <zackarydsteinmetz@gmail.com>
2021-04-07 16:58:34 -0700

    Assest and Stuff

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,10 +1,32 @@
-//const { Phaser } = require("../../Library/phaser");
-
 class Play extends Phaser.Scene {
     constructor(){
         super("playScene");
     }
+
+    preload(){
+        // load images/title sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
     create(){
-        this.add.text(20, 20, "Rocket Patrol Play");
+        // place starfield
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
+       // green UI background
+       this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+       // white borders
+       this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+       this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+       this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+       this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
+       // add rocket (player 1)
+       this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+    }
+
+    update(){
+        this.starfield.tilePositionX -= starSpeed;
     }
 }
